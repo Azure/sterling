b68bd22af7d28d0c645a570faf3475e1c5ee1f0b
Drew Furgiuele <drew.furgiuele@microsoft.com>
2022-07-13 11:37:26 -0400

    correcting url paths

--- config/azure-file-storage/configure-azurefiles-driver.sh
+++ config/azure-file-storage/configure-azurefiles-driver.sh
@@ -5,10 +5,10 @@ clientSecret=$(cat ~/.azure/osServicePrincipal.json | jq -r .clientSecret)
 driver_version="1.19.0"
 
 #Create the azure.json file and upload as secret
-wget -nv https://raw.githubusercontent.com/Azure/sterling/$BRANCH_NAME/aro/azure.json -O /tmp/azure.json
+wget -nv https://raw.githubusercontent.com/Azure/sterling/$BRANCH_NAME/config/azure-file-storage/azure.json -O /tmp/azure.json
 envsubst < /tmp/azure.json > /tmp/azure.json
 export AZURE_CLOUD_SECRET=`cat /tmp/azure.json | base64 | awk '{printf $0}'; echo`
-wget -nv https://raw.githubusercontent.com/Azure/sterling/$BRANCH_NAME/aro/azure-cloud-provider.yaml -O /tmp/azure-cloud-provider.yaml
+wget -nv https://raw.githubusercontent.com/Azure/sterling/$BRANCH_NAME/config/azure-file-storage/azure-cloud-provider.yaml -O /tmp/azure-cloud-provider.yaml
 envsubst < /tmp/azure-cloud-provider.yaml > /tmp/azure-cloud-provider.yaml
 oc apply -f /tmp/azure-cloud-provider.yaml
 
@@ -25,14 +25,14 @@ echo "Driver version " $driver_version
 curl -skSL https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/$driver_version/deploy/install-driver.sh | bash -s $driver_version --
 
  #Configure Azure Files Standard
- wget -nv https://raw.githubusercontent.com/Azure/sterling/$BRANCH_NAME/aro/azurefiles-standard.yaml -O /tmp/azurefiles-standard.yaml
+ wget -nv https://raw.githubusercontent.com/Azure/sterling/$BRANCH_NAME/config/azure-file-storage/azurefiles-standard.yaml -O /tmp/azurefiles-standard.yaml
  envsubst < /tmp/azurefiles-standard.yaml > /tmp/azurefiles-standard.yaml
  sudo -E oc apply -f /tmp/azurefiles-standard.yaml
 
 #Deploy premium Storage Class
- wget -nv https://raw.githubusercontent.com/Azure/sterling/$BRANCH_NAME/aro/azurefiles-premium.yaml -O /tmp/azurefiles-premium.yaml
+ wget -nv https://raw.githubusercontent.com/Azure/sterling/$BRANCH_NAME/config/azure-file-storage/azurefiles-premium.yaml -O /tmp/azurefiles-premium.yaml
  envsubst < /tmp/azurefiles-premium.yaml > /tmp/azurefiles-premium.yaml
  sudo -E oc apply -f /tmp/azurefiles-premium.yaml
 
 #Deploy volume binder
-sudo -E /tmp/OCPInstall/oc apply -f https://raw.githubusercontent.com/Azure/sterling/$BRANCH_NAME/aro/persistent-volume-binder.yaml
+sudo -E /tmp/OCPInstall/oc apply -f https://raw.githubusercontent.com/Azure/sterling/$BRANCH_NAME/config/azure-file-storage/persistent-volume-binder.yaml
