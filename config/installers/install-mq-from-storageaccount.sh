3333dd9464abbe3a55746f9844874fabd3d3eb70
Drew Furgiuele <drew.furgiuele@microsoft.com>
2022-07-20 09:43:08 -0400

    mq installer changes

--- config/installers/install-mq-from-storageaccount.sh
+++ config/installers/install-mq-from-storageaccount.sh
@@ -1,5 +1,5 @@
 #Download MQ Installer. Environment variables set in cloudinit of VM
-echo $INSTALL_SAS_TOKEN
+echo $INSTALLER_SAS_TOKEN
 sudo /tmp/azcopy/azcopy copy  "https://$INSTALLER_STORAGEACCOUNT_NAME.blob.core.windows.net/$INSTALLER_STORAGECONTAINER_NAME/$MQ_INSTALLER_ARCHIVE_FILENAME?$INSTALLER_SAS_TOKEN" /tmp/mq.tar.gz
 sudo tar -xf /tmp/mq.tar.gz -C /mnt
 sudo rm /tmp/mq.tar.gz
