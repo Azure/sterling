e978707223803be3ae8a1d3df3335fbbc65d49c3
Drew Furgiuele <drew.furgiuele@microsoft.com>
2022-08-30 09:48:55 -0400

    adding psql tools to jumpbox

---
+++ config/installers/setup-pgsql-tools.sh
@@ -0,0 +1,3 @@
+sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo dnf -qy module disable postgresql
+sudo dnf install -y postgresql14-server
